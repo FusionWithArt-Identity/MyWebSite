8c5cf06696d1de63a441e14ad85acbb959d54778
FusionWithArt-Identity <mrigadas30@gmail.com>
2025-10-26 11:49:52 +0530

    Update main.js

--- main.js
+++ main.js
@@ -517,7 +517,7 @@ document.addEventListener('DOMContentLoaded', function() {
             title: 'Special Practice Session by Dr. Mahesh Khetmalis Video',
             text: '<p>Video from a special practice session conducted by Dr. Mahesh Khetmalis.</p>',
             media: [
-                { type: 'video', src: 'https.w3schools.com/html/mov_bbb.mp4', title: 'Dr. Khetmalis Session' }
+                { type: 'video', src: 'https://www.w3schools.com/html/mov_bbb.mp4', title: 'Dr. Khetmalis Session' }
             ]
         },
         'videos-practice-dr-jewel': {
